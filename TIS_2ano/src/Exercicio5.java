38885a2f4eae6d18f63a6a93dfb3d4af722ddc66
Duarte Cota <duartenunocota@gmail.com>
2020-10-21 09:05:50 +0000

    update 20 out 2020

--- TIS_2ano/src/Exercicio5.java
+++ TIS_2ano/src/Exercicio5.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
 
 public class Exercicio5 {
-
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.print("Introduza um numero: ");
+		System.out.print("Introduza um número: ");
 		int num = input.nextInt();
 		if( num % 2 == 0) {
-			System.out.println("O numero e PAR");
+			System.out.println("O número é PAR");
 		} else {
-			System.out.println("O numero e IMPAR");
+			System.out.println("O número é IMPAR");
 		}
 		input.close();
 	}
---
+++ TIS_2ano/src/ResAula.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ResAula{
+    public static void main(String[] args){
+        int[] array = {1,2,3,4,5,67,89}; //array fixo
+        int[] numeros = new int[100]; //array vazio
+        int comprimento = array.length;
+        System.out.println("Comprimento do array: " +comprimento);
+        System.out.println(array[5]);
+    }
+}
