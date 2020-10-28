c172999410ce8a2203cac6f131def1d2fce30aa1
Duarte Cota <duartenunocota@gmail.com>
2020-10-28 09:14:55 -0100

    update 28 out 2020

--- TIS_2ano/src/Exercicio5.java
+++ TIS_2ano/src/Exercicio5.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 
 public class Exercicio5 {
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Introduza um numero: ");
-		int num = input.nextInt();
-		if( num % 2 == 0) {
-			System.out.println("O numero e PAR");
+		Scanner input = new Scanner(System.in); //passo1
+		System.out.print("Introduza um numero: ");//passo1
+		int num = input.nextInt();//passo1
+		int resto = num % 2; //passo2
+		if( resto == 0) { //passo2
+			System.out.println("O numero e PAR"); //passo3
 		} else {
-			System.out.println("O numero e IMPAR");
+			System.out.println("O numero e IMPAR"); //passo3
 		}
 		input.close();
 	}
