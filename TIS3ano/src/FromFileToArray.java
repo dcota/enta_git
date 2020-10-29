d34a26eb4e73ff6617a18d3aad3fd89de394b322
Duarte Cota <duartenunocota@gmail.com>
2020-10-29 16:40:50 -0100

    update 29out2020

--- TIS3ano/src/FromFileToArray.java
+++ TIS3ano/src/FromFileToArray.java
@@ -1,10 +1,8 @@
 import java.util.*;
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.io.FileNotFoundException;
 
-public class Excecao {
+public class FromFileToArray {
 	private static int[] numAluno = new int[20];
 	private static String[] primNomeAluno = new String[20];
 	private static String[] ultNomeAluno = new String[20];
@@ -15,12 +13,16 @@ public class Excecao {
 	public static void main(String[] args) {
 		String[] linha = null;
 		try {
+			//ler do ficheiro
 			File f = new File("ficheiro.txt");
 			Scanner scan = new Scanner(f);
 			int index = 0;
+			//enquanto houver linhas processar cada linha
 			while(scan.hasNextLine()) {
 				String data = scan.nextLine();
+				//faz split da string usando o separador ";"
 				linha = data.split(";");
+				//copia os dados do array linha para cada array de dados
 				numAluno[index] = Integer.parseInt(linha[0]);
 				primNomeAluno[index] = linha[1];
 				ultNomeAluno[index] = linha[2];
@@ -28,23 +30,39 @@ public class Excecao {
 				freguesia[index] = linha[4];
 				concelho[index] = linha[5];
 				index++;	
-			}			
-		} catch (Exception e) {
+			}	
+			scan.close();
+		} catch (FileNotFoundException e) {
 			System.out.print("Ficheiro n encontrado...");
 		}	
-		printArrays();
+		//mostrar o conteúdo dos arrays de dados para teste
+		printArrays();	
 	}
 	
+	//método para mostrar os arrays
 	static void printArrays() {
 		for(int num : numAluno) {
 			System.out.print(num + " ");	
 		}
-		/*for(int i=0; i<numAluno.length; i++) {
-			System.out.print(numAluno[i] + " ");
-		}*/
 		System.out.println("");
-		for(String s : primNomeAluno) {
-			System.out.print(s + " ");	
+		for(String primNom : primNomeAluno) {
+			System.out.print(primNom + " ");	
+		}
+		System.out.println("");
+		for(String ultNom : ultNomeAluno) {
+			System.out.print(ultNom + " ");	
+		}
+		System.out.println("");
+		for(int idade : idade) {
+			System.out.print(idade + " ");	
+		}
+		System.out.println("");
+		for(String freg : freguesia) {
+			System.out.print(freg + " ");	
+		}
+		System.out.println("");
+		for(String conc : concelho) {
+			System.out.print(conc + " ");	
 		}
 		System.out.println("");
 	}
