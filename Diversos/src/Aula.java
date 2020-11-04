e2b4cf90a4d0d30a26a142f06cbf3bec6c71e3b9
duarte cota <duartenunocota@gmail.com>
2020-11-04 21:35:30 -0100

    update 4nov2020

--- Diversos/src/Aula.java
+++ Diversos/src/Aula.java
@@ -1,15 +1,20 @@
 import java.util.*;
 
 public class Aula {
-    public static void main(String[] args){
-        Scanner op = new Scanner (System.in);
-        System.out.println("MENU");
-        System.out.println("1 - Opcao 1");
-        System.out.println("2 - Opcao 2");
-        System.out.println("3 - Opcao 3");
-        System.out.println("4 - Opcao 4");
-        System.out.print("Escolher opcao: " );
-        int opc = op.nextInt();
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> lista = new ArrayList<Integer>(Arrays.asList(2,5,4,6,2));
+        ArrayList<Integer> listaFinal = new ArrayList<Integer>(Arrays.asList());
+        
+        listaFinal.add(lista.get(0));
+        
+        for(int i=0; i<lista.size()-1; i++)
+        {
+              listaFinal.add(i+1, lista.get(i+1)-lista.get(i));
+        }
+        for (int i=0; i<5; i++)
+        {
+            System.out.printf("%d\t",listaFinal.get(i));
+        }
     }
 }
-
