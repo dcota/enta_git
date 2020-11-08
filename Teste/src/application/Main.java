51e3812697e53d906ed987ea3e71dd7e733ed5e5
duarte cota <duartenunocota@gmail.com>
2020-11-08 21:41:10 -0100

    update 8nov2020

--- Teste/src/application/Main.java
+++ Teste/src/application/Main.java
@@ -1,17 +1,37 @@
 package application;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root, 600, 400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			//BorderPane root = new BorderPane();
+			//Scene scene = new Scene(root, 600, 400);
+			
+			Button b = new Button("Click");
+			Button exit = new Button("EXIT");
+			b.setOnAction(e -> System.out.println("Hello"));
+			exit.setOnAction(e -> {
+				System.out.println("A terminar...");
+				System.exit(0);
+			});
+			
+			VBox root = new VBox();
+			root.getChildren().addAll(b,exit);
+			
+			
+			Scene scene = new Scene(root,600,400);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("MyTitle");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch (Exception e) {
