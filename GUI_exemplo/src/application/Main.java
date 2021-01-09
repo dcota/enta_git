b7a495ff4af7d4741526908d066ac609780d988b
duarte cota <duartenunocota@gmail.com>
2021-01-09 21:17:38 -0100

    update

--- GUI_exemplo/src/application/Main.java
+++ GUI_exemplo/src/application/Main.java
@@ -4,10 +4,6 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-<<<<<<< HEAD:GUI/src/application/Main.java
-import javafx.scene.layout.BorderPane;
-=======
->>>>>>> d7428e7e85d1944656a80f2a0d4028ea3aada1ed:GUI_Intro/src/application/Main.java
 import javafx.fxml.FXMLLoader;
 
 
@@ -15,13 +11,8 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-<<<<<<< HEAD:GUI/src/application/Main.java
-			AnchorPane root = (AnchorPane)FXMLLoader.load(Main.class.getResource("Sample.fxml"));
-			Scene scene = new Scene(root);
-=======
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,400,400);
->>>>>>> d7428e7e85d1944656a80f2a0d4028ea3aada1ed:GUI_Intro/src/application/Main.java
+			Scene scene = new Scene(root,600,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
