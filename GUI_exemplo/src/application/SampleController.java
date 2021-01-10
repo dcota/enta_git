f94a162895f2944ac40284978df46ea3fec5bcd2
duarte cota <duartenunocota@gmail.com>
2021-01-10 21:39:25 -0100

    update

--- GUI_exemplo/src/application/SampleController.java
+++ GUI_exemplo/src/application/SampleController.java
@@ -1,11 +1,18 @@
 package application;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.ToggleGroup;
 
 public class SampleController {
 	
@@ -32,14 +39,44 @@ public class SampleController {
 
     @FXML
     private Label label2;
+    
+    @FXML
+    private Label lab3;
+    
+    @FXML
+    private RadioButton radio1;
 
     @FXML
-    void choice_chkBox(ActionEvent event) {
-    	label2.setText(chkBox.getValue());
-    }
+    private RadioButton radio2;
 
     @FXML
-    void encomenda(ActionEvent event) {
+    private RadioButton radio3;
+
+    @FXML
+    private RadioButton radio4;
+    
+    private ToggleGroup toggleGroup;
+    
+    @FXML
+    private Label lab4;
+    
+    @FXML
+    private ComboBox<String> comboBox;
+    
+    @FXML
+    private ListView<String> itemList1;
+
+    @FXML
+    private Button itensBtn;
+
+    @FXML
+    private TextArea textField1;
+    
+    @FXML
+    private Button clearTF;
+
+    @FXML
+    public void encomenda(ActionEvent event) {
     	String encomenda="Pizzas encomendadas:\n";
     	if(op1.isSelected()) {
     		encomenda+="Peperoni\n";
@@ -53,10 +90,72 @@ public class SampleController {
     	opc.setText(encomenda);
     }
     
+    @FXML
+    void choice_chkBox(ActionEvent event) {
+    	this.label2.setText(chkBox.getValue());
+    }
+    
+    @FXML
+    public void comboBox(ActionEvent event) {
+    	this.lab3.setText(comboBox.getValue());
+    }
+    
+     @FXML    
+    public void radioBtnChanged(ActionEvent event) {
+    	if(this.toggleGroup.getSelectedToggle().equals(this.radio1)) {
+    		lab4.setText("Java");
+    	}
+    	
+    	if(this.toggleGroup.getSelectedToggle().equals(this.radio2)) {
+    		lab4.setText("PHP");
+    	}
+    	
+    	if(this.toggleGroup.getSelectedToggle().equals(this.radio3)) {
+    		lab4.setText("C++");
+    	}
+    	
+    	if(this.toggleGroup.getSelectedToggle().equals(this.radio4)) {
+    		lab4.setText("JavaScript");
+    	}
+    }
+    
+    @FXML
+    public void select(ActionEvent event) {
+    	String textAreaString = "";
+    	ObservableList<String> list = itemList1.getSelectionModel().getSelectedItems();
+    	for(Object item : list) {
+    		textAreaString += String.format("%s%n", (String)item);
+    	}
+    	textField1.setText(textAreaString);
+    }
+    
+    @FXML
+    public void clearTF(ActionEvent event) {
+    	String textAreaString = "";
+    	textField1.setText(textAreaString);
+    }
+    
+    
     public void initialize() {
-     	chkBox.getItems().add("Duarte");
-    	chkBox.getItems().add("Ana");
-    	chkBox.getItems().add("Carlos");
+    	//preencher choiceBox
+     	this.chkBox.getItems().add("Duarte");
+    	this.chkBox.getItems().add("Ana");
+    	this.chkBox.getItems().add("Carlos");
+    	
+    	//prencher comboBox
+    	this.comboBox.getItems().addAll("op1", "op2", "op3");
+    	
+    	//criar ToggleGroup para os Radio Button 
+    	this.toggleGroup = new ToggleGroup();
+    	this.radio1.setToggleGroup(toggleGroup);
+    	this.radio2.setToggleGroup(toggleGroup);
+    	this.radio3.setToggleGroup(toggleGroup);
+    	this.radio4.setToggleGroup(toggleGroup);
+    	
+    	//criar lista de itens
+    	this.itemList1.getItems().addAll("Lápis", "Canetas", "Borrachas", "PapelA4", "Lapiseira", "Régua");
+    	this.itemList1.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);    	
+    	
     }
 
 }
