21d890c32c40da8b70aba7ab2987fa1e28682373
duarte cota <duartenunocota@gmail.com>
2021-01-12 13:01:22 -0100

    update

--- GUIExemploAula/src/application/Main.java
+++ GUIExemploAula/src/application/Main.java
@@ -3,8 +3,6 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.control.TabPane;
-import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
@@ -14,11 +12,8 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root);
+			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			Image icon = new Image("icon.png");
-			primaryStage.getIcons().add(icon);
-			primaryStage.setTitle("Soma");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
